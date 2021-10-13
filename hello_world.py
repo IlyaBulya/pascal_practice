42b3a964213fe0b829451843c7e68d2100d4bf73
Ilya <ilyade3004@gmail.com>
2021-10-13 17:03:54 +0500

    test merge

--- hello_world.py
+++ hello_world.py
@@ -3,6 +3,9 @@ def func():
     a = 4 + 5
     print(f'Hello Wold!: {a}')
 
+def func2():
+    print("New function")
+
 # аналог функции main в с++ и читый бегин из паскаля
 if __name__ == "__main__":
-    func()
+    func2()
